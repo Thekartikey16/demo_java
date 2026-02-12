534a3dc679acf3584bdf7a2c7b13c53b6767d8e3
Thekartikey16 <singhkartik9450@gmail.com>
2026-02-12 12:07:23 +0530

    add floder

--- incre.java
+++
@@ -1,8 +0,0 @@
-public class incre {
-    public static void main(String[] args) {
-        int x = 3;
-        int c = x++ + ++x;
-        // int d = x++ + x++ ;
-        System.out.println(c +"\n" );
-    }
-}
--- main1.java
+++
@@ -1,9 +0,0 @@
-public class main1 {
- public static void main(String[] args) {
-     rectangle R = new rectangle();
-     R.getdata(3 , 5);
-     int res  = R.area();
-     System.out.println("The area is " +res);
-    }
-}
-
